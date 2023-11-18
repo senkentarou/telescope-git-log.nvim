22e8d60cb89b7047ca434a9991dc7d3a875cd521
senkentarou <masahiro-senda@c-fo.com>
2023-11-18 09:21:02 +0900

    refactor

--- lua/git_log/builtin.lua
+++ lua/git_log/builtin.lua
@@ -21,18 +21,18 @@ B.list_commits_on_file = function(opts)
 
   pickers.new(opts, {
     results_title = 'commits on ' .. opts.current_file,
-    prompt_title = 'list commits on file',
+    prompt_title = '<C-y>:copy/<C-o>:blob/<C-p>:compare/<CR>:commit',
     finder = git_log_f.content_finder(opts),
     previewer = git_log_p.content_previewer(),
     sorter = config.file_sorter(opts),
     attach_mappings = function(_, map)
-      map("n", "o", git_log_a.view_blob(opts))
-      map("n", "p", git_log_a.view_compare(opts))
-      map("n", "y", git_log_a.copy_hash)
+      map('n', 'y', git_log_a.copy_hash)
+      map('n', 'o', git_log_a.view_blob(opts))
+      map('n', 'p', git_log_a.view_compare(opts))
 
-      map("i", "<C-o>", git_log_a.view_blob(opts))
-      map("i", "<C-p>", git_log_a.view_compare(opts))
-      map("i", "<C-y>", git_log_a.copy_hash)
+      map('i', '<C-y>', git_log_a.copy_hash)
+      map('i', '<C-o>', git_log_a.view_blob(opts))
+      map('i', '<C-p>', git_log_a.view_compare(opts))
 
       -- <CR> action
       actions.select_default:replace(git_log_a.view_commit(opts))
